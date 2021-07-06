b717e18de445dfba80b22d2fc6702e0b247942ed
LukeBailey181 <lukebailey@college.harvard.edu>
2021-07-06 15:46:36 +0100

    add image state

--- src/componants/pages/home.js
+++ src/componants/pages/home.js
@@ -7,6 +7,30 @@ export default function HomePage(props) {
 
   const display = {"backgroundColor":"blue"}
 
+  [image_1, setImage1] = useState({
+    title : "mountains",
+    image : require("../../assets/mountain.jpg"),
+    selected : false
+  })
+
+  [image_2, setImage2] = useState({
+    title : "city",
+    image : require("../../assets/city.jpg"),
+    selected : false
+  })
+
+  [image_3, setImage3] = useState({
+    title : "sunset",
+    image : require("../../assets/sunset.jpg"),
+    selected : false
+  })
+  
+  [image_4, setImage4] = useState({
+    title : "snow",
+    image : require("../../assets/snow.jpg"),
+    selected : false
+  })
+
   return (
     <div className="full-screen-container">
       <Grid container direction="column" align="center" xs={12}>
