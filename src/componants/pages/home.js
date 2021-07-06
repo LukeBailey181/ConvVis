e5926d5622011229cea2bb74f765a7efe6229dee
LukeBailey181 <lukebailey@college.harvard.edu>
2021-07-06 13:06:33 +0100

    create file structure and abstract homepage

---
+++ src/componants/pages/home.js
@@ -0,0 +1,14 @@
+import React, { component, useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+export default function HomePage(props) {
+
+  return (
+    <div className="App">
+      <h1>
+        CONVOLUTION VISUALIZATION APP
+      </h1>
+      This is some text
+    </div>
+  )
+}
